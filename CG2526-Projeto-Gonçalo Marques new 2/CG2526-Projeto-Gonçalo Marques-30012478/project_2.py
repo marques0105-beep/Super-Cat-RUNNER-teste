40bada8addffff8410d2cae15367f60f2ce2c4fd
marques0105-beep <joao23.goncalomarques0105@gmail.com>
2026-01-21 23:48:48 +0000

    Update project_2.py

--- CG2526-Projeto-Gonçalo Marques new 2/CG2526-Projeto-Gonçalo Marques-30012478/project_2.py
+++ CG2526-Projeto-Gonçalo Marques new 2/CG2526-Projeto-Gonçalo Marques-30012478/project_2.py
@@ -1,4 +1,3 @@
-# project_2.py
 import pygame
 import sys
 import random
@@ -339,3 +338,4 @@ if __name__ == "__main__":
     # Importa e inicia o menu
     from interfaces import menu
     menu(screen)
+
